58dfd636f2bf8d2ebc362997e94eb09ca8c99415
Alex <ajollid@gmail.com>
2018-04-16 21:35:28 -0700

    Updates app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+
+  constructor(){
+    super();
+    this.state =
+    {
+        currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
+        messages: []
+      }
+      //Bind functions
+      this.handleMessage = this.handleMessage.bind(this);
+    }
+
+    handleMessage(event){
+      if(event.key === 'Enter'){
+        let userName = this.state.currentUser.name;
+        let newMessage = {username: userName, content: event.target.value}
+        event.target.value = '';
+        this.socket.send(JSON.stringify(newMessage));
+      }
+    }
+
+    componentDidMount() {
+      this.socket = new WebSocket('ws://localhost:3001', 'protocolOne');
+      this.socket.onmessage = (event) => {
+        let message = JSON.parse(event.data);
+        let oldMessages = this.state.messages;
+        let newMessages = [...oldMessages, message];
+        this.setState({messages: newMessages});
+      }
+    }
+
+    render() {
+      return (
+        <div>
+        <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser} handleMessage={() => this.handleMessage}/>
+        </div>
+        );
+    }
   }
-}
-export default App;
+  export default App;
---
+++ src/ChatBar.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ChatBar extends Component {
+
+  render() {
+    return (
+        <footer className="chatbar">
+          <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name}/>
+          <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyDown={this.props.handleMessage('test')}/>
+        </footer>
+    );
+  }
+}
+export default ChatBar;
