2b6ba6b2bf30e190d7ccaac2ea68d885cc25c0a8
Alex Holliday <ajhollid@gmail.com>
2018-04-18 21:02:20 +0000

    Implemented multiple rooms

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
@@ -16,13 +18,15 @@ class App extends Component {
     this.TYPE_POST_DISCONNECT = 'postDisconnect';
     this.TYPE_INCOMING_DISCONNECT = 'incomingDisconnect';
 
-    this.state =
-      {
-        currentUser: { name: 'Anonymous' }, // optional. if currentUser is not defined, it means the user is Anonymous
-        messages: [],
-        notifications: [],
-        userCount: 0,
-      }
+    this.state = {
+      currentUser: {
+        name: 'Anonymous'
+      }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [],
+      notifications: [],
+      userCount: 0,
+      room: 0,
+    }
     //Bind functions
     this.handleMessage = this.handleMessage.bind(this);
     this.setUser = this.setUser.bind(this);
@@ -30,16 +34,34 @@ class App extends Component {
 
   setUser(username) {
     let oldUsername = this.state.currentUser.name;
-    this.setState({ currentUser: { name: username } });
+    this.setState({
+      currentUser: {
+        name: username
+      }
+    });
     let msgString = oldUsername + ' has changed their name to ' + username;
-    let message = { type: this.TYPE_POST_NOTIFICATION, content: msgString };
+    let message = {
+      type: this.TYPE_POST_NOTIFICATION,
+      content: msgString,
+      room: -1,
+    };
     this.postNotification(message);
+  }
+
+  setRoom(event, room){
+    event.preventDefault();
+    this.setState({room})
   }
 
   handleMessage(event) {
     if (event.key === 'Enter') {
       let userName = this.state.currentUser.name;
-      let newMessage = { type: 'postMessage', username: userName, content: event.target.value }
+      let newMessage = {
+        type: 'postMessage',
+        username: userName,
+        content: event.target.value,
+        room: this.state.room,
+      }
       event.target.value = '';
       this.socket.send(JSON.stringify(newMessage));
     }
@@ -49,11 +71,11 @@ class App extends Component {
     this.socket.send(JSON.stringify(message));
   }
 
-  concatNewMessages(message){
+  concatNewMessages(message) {
     return this.state.messages.concat(message);
   }
 
-  concatNewNotification(notification){
+  concatNewNotification(notification) {
     return this.state.notifications.concat(notification);
   }
 
@@ -62,26 +84,33 @@ class App extends Component {
     this.socket.onmessage = (event) => {
       let message = JSON.parse(event.data);
       switch (message.type) {
-        case this.TYPE_INCOMING_MESSAGE: {
-          let newMessages = this.concatNewMessages(message);
-          this.setState({ messages: newMessages });
-          break;
-        }
-        case this.TYPE_INCOMING_NOTIFICATION: {
-          let newNotifications = this.concatNewNotification(message);
-          this.setState({ notifications: newNotifications });
-          break;
-        }
+        case this.TYPE_INCOMING_MESSAGE:
+          {
+            let newMessages = this.concatNewMessages(message);
+            this.setState({
+              messages: newMessages
+            });
+            break;
+          }
+        case this.TYPE_INCOMING_NOTIFICATION:
+          {
+            let newNotifications = this.concatNewNotification(message);
+            this.setState({
+              notifications: newNotifications
+            });
+            break;
+          }
 
         case this.TYPE_INCOMING_CONNECT:
-        case this.TYPE_INCOMING_DISCONNECT: {
-          let newNotifications = this.concatNewNotification(message);
-          this.setState(
-            { notifications: newNotifications,
+        case this.TYPE_INCOMING_DISCONNECT:
+          {
+            let newNotifications = this.concatNewNotification(message);
+            this.setState({
+              notifications: newNotifications,
               userCount: message.userCount
             });
-          break;
-        }
+            break;
+          }
       }
     }
   }
@@ -90,9 +119,14 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a><p className="navbar-usercount">{this.state.userCount} users online</p>
+          <a href="/" className="navbar-brand">Chatty</a>
+          <ul>
+            <li className={this.state.room === 0 ? 'selected' : ''}><a href="" onClick={(e) => {this.setRoom(e, 0)}}>Room 1</a></li>
+            <li className={this.state.room === 1 ? 'selected' : ''}><a href="" onClick={(e) => {this.setRoom(e, 1)}}>Room 2</a></li>
+          </ul>
+          <p className="navbar-usercount">{this.state.userCount} users online</p>
         </nav>
-        <MessageList messages={this.state.messages} notifications={this.state.notifications} color={this.state.color} />
+        <MessageList messages={this.state.messages} notifications={this.state.notifications} color={this.state.color} room={this.state.room} />
         <ChatBar currentUser={this.state.currentUser} handleMessage={() => this.handleMessage} setUser={this.setUser} />
       </div>
     );
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -9,13 +9,19 @@ class MessageList extends Component {
     const notifications = this.props.notifications;
     const messages = this.props.messages;
     const combinedArray = notifications.concat(messages);
+    const room = this.props.room;
+
     combinedArray.sort((a, b) => {
       if (a.id < b.id) return -1;
       if( a.id > b.id) return 1;
       return 0;
     })
 
-    const sortedMessages = combinedArray.map((message) => {
+    const filteredArray = combinedArray.filter( (message) => {
+      return message.room === -1 || message.room === room;      
+    });
+
+    const sortedMessages = filteredArray.map((message) => {
       return message.type === 'incomingMessage' 
       ? <Message key={message.id} message={message} /> 
       : <Notification key={message.id} message={message} />;
